08228b814107be5e1ae9b876ecd77b3346ae49c0
flverge_42 <verge.f@gmail.com>
2023-11-20 09:46:56 +0100

    Init .h and main.c

---
+++ fractol.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/20 09:00:32 by flverge           #+#    #+#             */
+/*   Updated: 2023/11/20 09:07:15 by flverge          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include <fcntl.h> // open and options
+# include <unistd.h> // write, close, access, dup, dup2, execve
+# include <stdlib.h> // malloc and free
+# include <stdio.h> // perror
+# include <errno.h> // makes errno a global variable usable with strerror(errno)
+# include <math.h>  // Authorized functions
+
+# include <mlx.h> // MiniLibX
+
+
+
+
+
+#endif 
