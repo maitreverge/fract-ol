11bf374a661ad574c46e5d6df6b069aaf27c0fd2
flverge_42 <verge.f@gmail.com>
2023-11-28 13:18:44 +0100

    args checker ok, need more secured functions

--- src/args_checker.c
+++ src/args_checker.c
@@ -6,7 +6,7 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 11:37:12 by flverge           #+#    #+#             */
-/*   Updated: 2023/11/28 12:23:09 by flverge          ###   ########.fr       */
+/*   Updated: 2023/11/28 13:17:55 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,35 +38,35 @@ void	display_options(int num_err)
 int	first_arg(char *av1) // working
 {
 	// ? m/M - j/J - x/X full uppercase or check double possibilities
-	if (ft_strncmp(av1, "J", 1) && ft_strncmp(av1, "M", 1) && ft_strncmp(av1, "X", 1) )
+	if (ft_strlen(av1) == 1 && ft_strncmp(av1, "J", 1) && ft_strncmp(av1, "M", 1) && ft_strncmp(av1, "X", 1) )
 		return 1;
 	return 0;
 }
 
 int		second_arg_j(char **av)
 {
-	if (av[1] == "J")
+	if (!ft_strncmp(av[1], "J", 1) )
 	{
-		if (av[2] != "1" || av[2] != "2" || av[2] != "3")
-		// ! WORKING HERE
-		if (ft_strncmp(av[2], "1", 1))
+		if (!av[2])
+			return 1;
+		if (ft_strncmp(av[2], "1", 1) && ft_strncmp(av[2], "2", 1) && ft_strncmp(av[2], "3", 1))
 			return 1;
 	}
 	return (0);
 }
 
-int third_arg(int ac, char **av)
+int third_arg(int ac, char **av)  // potential chash between "R" and "Rainbow" arguments
 {
 	// if pas de J et des couleurs
-	if (av[1] != "J" && ac == 3)
+	if (ft_strncmp(av[1], "J", 1) && ac == 3)
 	{
-		if (av[2] != "R" && av[2] != "G" && av[2] != "B" && ft_strncmp(av[2], "Rainbow", 7) && ft_strncmp(av[2], "LSD", 3))
+		if (ft_strncmp(av[2], "R", 1) && ft_strncmp(av[2], "G", 1) && ft_strncmp(av[2], "B", 1) && ft_strncmp(av[2], "Rainbow", 7) && ft_strncmp(av[2], "LSD", 3))
 			return 1;
 	}
 	// if J et des couleurs
-	if (av[1] == "J" && ac == 4)
+	if (!ft_strncmp(av[1], "J", 1) && ac == 4)
 	{
-		if (av[3] != "R" && av[3] != "G" && av[3] != "B" && ft_strncmp(av[3], "Rainbow", 7) && ft_strncmp(av[3], "LSD", 3))
+		if (ft_strncmp(av[3], "R", 1) && ft_strncmp(av[3], "G", 1) && ft_strncmp(av[3], "B", 1) && ft_strncmp(av[3], "Rainbow", 7) && ft_strncmp(av[3], "LSD", 3))
 			return 1;
 	}
 	return (0);
