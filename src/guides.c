6ffbb1cfea5c3e6e373dc341341fe1105826d68e
flverge_42 <verge.f@gmail.com>
2023-12-05 17:17:15 +0100

    MINOR UPDATE : add second guide

--- src/guides.c
+++ src/guides.c
@@ -6,7 +6,7 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 17:02:21 by flverge           #+#    #+#             */
-/*   Updated: 2023/12/05 17:05:17 by flverge          ###   ########.fr       */
+/*   Updated: 2023/12/05 17:15:19 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,16 @@ void	display_guide_1(t_vars *vars)
 
 void	display_guide_2(t_vars *vars)
 {
-	mlx_string_put(vars->mlx, vars->win, 250, 25, BLACK, "--------------------");
+	mlx_string_put(vars->mlx, vars->win, 450, 25, BLACK, "--------------------");
+	mlx_string_put(vars->mlx, vars->win, 450, 130, BLACK, "--------------------");
+	for (int i = 28; i < 130; i+=9)
+		mlx_string_put(vars->mlx, vars->win, 448, i, BLACK, "|");
+	for (int i = 28; i < 130; i+=9)
+		mlx_string_put(vars->mlx, vars->win, 573, i, BLACK, "|");
+	mlx_string_put(vars->mlx, vars->win, 460, 35, RED, "Arrows Keys :");
+	mlx_string_put(vars->mlx, vars->win, 460, 55, RED, "-> : Move Left");
+	mlx_string_put(vars->mlx, vars->win, 460, 75, RED, "<- : Move Right");
+	mlx_string_put(vars->mlx, vars->win, 460, 95, RED, "^  : Move Up");
+	mlx_string_put(vars->mlx, vars->win, 460, 115, RED, "v  : Move Down");
 	
 }
