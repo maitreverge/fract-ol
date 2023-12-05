21b5c7b15e8f363ee453208641394ca102816c37
flverge_42 <verge.f@gmail.com>
2023-12-05 11:26:15 +0100

    HOTFIX : fractal stop freezing when definition < 0

--- src/hooks.c
+++ src/hooks.c
@@ -6,7 +6,7 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 14:52:35 by flverge           #+#    #+#             */
-/*   Updated: 2023/12/04 17:52:53 by flverge          ###   ########.fr       */
+/*   Updated: 2023/12/05 11:25:04 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,15 +30,39 @@ int	key_listener(int keycode, t_vars *vars)
 		vars->shift_x += (0.5 * vars->original_zoom);
 	else if (keycode == RIGHT_KEY)
 		vars->shift_x -= (0.5 * vars->original_zoom);
+    if (vars->definition <= 0)
+		vars->definition = 1;
 	print_fractal(vars);  
 }
 
+// ! classic mouse zooming
+// int	mouse_listener(int mouseclick, int x, int y, t_vars *vars)
+// {
+//     if (mouseclick == 4)
+//         vars->original_zoom *= 0.8; // zoom in
+//     else if (mouseclick == 5)
+//         vars->original_zoom *= 1.2; // zoom out
+    
+//     print_fractal(vars);
+//     return (0);
+// }
+
 int	mouse_listener(int mouseclick, int x, int y, t_vars *vars)
 {
+    double zoom_factor;
     if (mouseclick == 4)
-        vars->original_zoom *= 0.8; // zoom in
+        zoom_factor = 0.8; // zoom in
     else if (mouseclick == 5)
-        vars->original_zoom *= 1.2; // zoom out
+        zoom_factor = 1.2; // zoom out
+
+    // Calculate the new zoom level
+    double new_zoom = vars->original_zoom * zoom_factor;
+
+    // Calculate the new shift values based on the mouse position and the new zoom level
+    vars->shift_x = (x / new_zoom) - ((x - vars->shift_x) / vars->original_zoom);
+    vars->shift_y = (y / new_zoom) - ((y - vars->shift_y) / vars->original_zoom);
+
+    vars->original_zoom = new_zoom;
 
     print_fractal(vars);
     return (0);
