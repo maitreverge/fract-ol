fe017a925b6f51f8c4301edf07444923afcdd274
flverge_42 <verge.f@gmail.com>
2023-12-06 14:14:14 +0100

    improved guides

--- src/guides.c
+++ src/guides.c
@@ -6,7 +6,7 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 17:02:21 by flverge           #+#    #+#             */
-/*   Updated: 2023/12/06 12:21:41 by flverge          ###   ########.fr       */
+/*   Updated: 2023/12/06 14:13:12 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,55 @@
 
 void	display_guide_1(t_vars *vars)
 {
-	mlx_string_put(vars->mlx, vars->win, 30, 25, BLACK, "------------------");
-	mlx_string_put(vars->mlx, vars->win, 30, 105, BLACK, "------------------");
-	for (int i = 28; i < 105; i+=9)
-		mlx_string_put(vars->mlx, vars->win, 25, i, BLACK, "|");
-	for (int i = 28; i < 105; i+=9)
-		mlx_string_put(vars->mlx, vars->win, 150, i, BLACK, "|");
-	mlx_string_put(vars->mlx, vars->win, 40, 35, RED, "Controls Keys");
-	mlx_string_put(vars->mlx, vars->win, 40, 65, RED, " + Key : More HD");
-	mlx_string_put(vars->mlx, vars->win, 40, 85, RED, " - Key : Less HD");
+	int x;
+
+	mlx_string_put(vars->mlx, vars->win, 40, 35, WHITE, "DEFINITION");
+	mlx_string_put(vars->mlx, vars->win, 40, 65, WHITE, " + Key : More HD");
+	mlx_string_put(vars->mlx, vars->win, 40, 85, WHITE, " - Key : Less HD");
+	x = 360;
+	mlx_string_put(vars->mlx, vars->win, x + 10, 35, WHITE, "ARROW KEYS :");
+	mlx_string_put(vars->mlx, vars->win, x + 10, 65, WHITE, "-> : Move Left");
+	mlx_string_put(vars->mlx, vars->win, x + 10, 85, WHITE, "<- : Move Right");
+	mlx_string_put(vars->mlx, vars->win, x + 10, 105, WHITE, "^  : Move Up");
+	mlx_string_put(vars->mlx, vars->win, x + 10, 125, WHITE, "v  : Move Down");
+	x += 330;
+	mlx_string_put(vars->mlx, vars->win, x + 10, 35, WHITE, "COLORS CONTROL :");
+	mlx_string_put(vars->mlx, vars->win, x + 10, 65, WHITE, "Right Click: Shift Color");
+	mlx_string_put(vars->mlx, vars->win, x + 10, 85, WHITE, "Press 1 for Black and White");
+	mlx_string_put(vars->mlx, vars->win, x + 10, 105, WHITE, "Press 2 for Red");
+	mlx_string_put(vars->mlx, vars->win, x + 10, 125, WHITE, "Press 3 for Green");
+	mlx_string_put(vars->mlx, vars->win, x + 10, 145, WHITE, "Press 4 for Blue");
+	mlx_string_put(vars->mlx, vars->win, x + 10, 165, WHITE, "Press 5 for Wow");
+	mlx_string_put(vars->mlx, vars->win, x + 10, 185, WHITE, "Press 6 for LSD");
+	x += 330;
+	mlx_string_put(vars->mlx, vars->win, x + 10, 35, WHITE, "ZOOM CONTROLS");
+	mlx_string_put(vars->mlx, vars->win, x + 10, 65, WHITE, "Scroll Up = Zoom +");
+	mlx_string_put(vars->mlx, vars->win, x + 10, 85, WHITE, "Scroll Up = Zoom -");
+	mlx_string_put(vars->mlx, vars->win, x + 10, 105, WHITE, "SPACE KEY : Reset Zoom");
 }
 
 void	display_guide_2(t_vars *vars)
 {
-	mlx_string_put(vars->mlx, vars->win, 450, 25, BLACK, "------------------");
-	mlx_string_put(vars->mlx, vars->win, 450, 130, BLACK, "------------------");
-	for (int i = 28; i < 130; i+=9)
-		mlx_string_put(vars->mlx, vars->win, 448, i, BLACK, "|");
-	for (int i = 28; i < 130; i+=9)
-		mlx_string_put(vars->mlx, vars->win, 573, i, BLACK, "|");
-	mlx_string_put(vars->mlx, vars->win, 460, 35, RED, "Arrows Keys :");
-	mlx_string_put(vars->mlx, vars->win, 460, 55, RED, "-> : Move Left");
-	mlx_string_put(vars->mlx, vars->win, 460, 75, RED, "<- : Move Right");
-	mlx_string_put(vars->mlx, vars->win, 460, 95, RED, "^  : Move Up");
-	mlx_string_put(vars->mlx, vars->win, 460, 115, RED, "v  : Move Down");
+	int	x;
+
+	// x = 
+
+	x = 1250;
+	mlx_string_put(vars->mlx, vars->win, x + 10, 35, WHITE, "JULIA CONTROLS :");
+	mlx_string_put(vars->mlx, vars->win, x + 10, 65, WHITE, "Left Click: Shift Julia Set");
+	x = 1300;
+	if (vars->fractal_name == 'M')
+		mlx_string_put(vars->mlx, vars->win, x + 10, 1400, WHITE, "Fractal : MANDELBROT");
+	else if (vars->fractal_name == 'X')
+		mlx_string_put(vars->mlx, vars->win, x + 10, 1400, WHITE, "Fractal : TRICORN");
+	else if (vars->fractal_name == 'J')
+	{
+		if (vars->julia_set == '1')
+			mlx_string_put(vars->mlx, vars->win, x + 10, 1400, WHITE, "Fractal : Julia Set 1");
+		if (vars->julia_set == '2')
+			mlx_string_put(vars->mlx, vars->win, x + 10, 1400, WHITE, "Fractal : Julia Set 2");
+		if (vars->julia_set == '3')
+			mlx_string_put(vars->mlx, vars->win, x + 10, 1400, WHITE, "Fractal : Julia Set 3");
+	}
 }
+
--- src/hooks.c
+++ src/hooks.c
@@ -6,7 +6,7 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 14:52:35 by flverge           #+#    #+#             */
-/*   Updated: 2023/12/06 12:55:38 by flverge          ###   ########.fr       */
+/*   Updated: 2023/12/06 14:05:45 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,8 @@ int	key_listener(int keycode, t_vars *vars)
 		vars->definition = 1;
 	if (keycode == ECHAP_KEY)
 		win_close(vars);
+	if (keycode == SPACE_KEY)
+		vars->original_zoom = 1.0;
 	fractal_definition(keycode, vars);
 	fractal_move(keycode, vars);
 	fractal_hotswap(keycode, vars);
